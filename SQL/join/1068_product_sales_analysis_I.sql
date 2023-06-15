0ddf41d700420bd5d1bfd523c5a4a5488d33da3d
zhennanunc <matthewfznn@gmail.com>
2023-06-15 14:59:52 +0800

    update SQL questions

---
+++ SQL/join/1068_product_sales_analysis_I.sql
@@ -0,0 +1,3 @@
+SELECT product_name, year, price FROM Sales
+JOIN Product
+ON Product.product_id = Sales.product_id
---
+++ SQL/join/197_rising_temperature.sql
@@ -0,0 +1,3 @@
+SELECT w1.id FROM Weather w1, Weather w2
+WHERE w1.temperature > w2.temperature
+AND TO_DAYS(w1.RecordDate) - TO_DAYS(w2.RecordDate) = 1
